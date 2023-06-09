710938c1e671a19825156ca2509b9395b29c02c2
Ashutosh Sharma <ashutosh-sharma-22@users.noreply.github.com>
2023-06-09 20:36:54 +0530

    Added new function

--- Project1/Firstapp/views.py
+++ Project1/Firstapp/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request) :
     return HttpResponse("Hello, World") 
+
+#Adding a new function named by Ashutosh
+def Ashutosh(request) :
+    return HttpResponse("Hello , Ashutosh!")
