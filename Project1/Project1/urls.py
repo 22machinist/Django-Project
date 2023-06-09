dd96ada2bccafc64fca701b18375e706456c99ef
Ashutosh Sharma <ashutosh-sharma-22@users.noreply.github.com>
2023-06-09 20:21:13 +0530

    Configured

--- Project1/Project1/urls.py
+++ Project1/Project1/urls.py
@@ -19,5 +19,5 @@ from django.urls import include , path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('FirstProject/' , include("First.urls"))
+    path('Firstapp/' , include("Firstapp.urls"))
 ]
