8960a5266f5e49f5ff4c17e80954e102196375b1
Ashutosh Sharma <ashutosh-sharma-22@users.noreply.github.com>
2023-06-09 20:37:54 +0530

    Updated url path

--- Project1/Firstapp/urls.py
+++ Project1/Firstapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path    #This will import path
 from . import views             #This will import views from the current directory 
 urlpatterns = [
     #Defining the path that will be rendered on the request
-    path("",views.index, name="index")
+    path("",views.index, name="index"),
+    path("Ashutosh" , views.Ashutosh , name="Ashutosh")
 ]
