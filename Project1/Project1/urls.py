6f9d17bd29074d747acf374c21416b3b592dc581
Ashutosh Sharma <ashutosh-sharma-22@users.noreply.github.com>
2023-06-10 21:35:32 +0530

    Defined the url for newyear app

--- Project1/Project1/urls.py
+++ Project1/Project1/urls.py
@@ -19,5 +19,6 @@ from django.urls import include , path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Firstapp/' , include("Firstapp.urls"))
+    path('Firstapp/' , include("Firstapp.urls")),
+    path('newyear/' , include("newyear.urls"))
 ]
