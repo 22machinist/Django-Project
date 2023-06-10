192f39222ab6d79d362548c40f2a827ddd949f6f
Ashutosh Sharma <ashutosh-sharma-22@users.noreply.github.com>
2023-06-10 06:52:39 +0530

    Added the links

--- Project1/Firstapp/urls.py
+++ Project1/Firstapp/urls.py
@@ -3,5 +3,10 @@ from . import views             #This will import views from the current directo
 urlpatterns = [
     #Defining the path that will be rendered on the request
     path("",views.index, name="index"),
-    path("Ashutosh" , views.Ashutosh , name="Ashutosh")
+    path("Ashutosh" , views.Ashutosh , name="Ashutosh"),
+    path("David" , views.David , name="David") , 
+    path("Malan" , views.Malan , name="Malan"),
+    path("Brian" , views.Brian , name="Brian"),
+    path("Havard" , views.Havard , name="Havard"),
+    path("Mi" , views.Mi , name="Mi")
 ]
