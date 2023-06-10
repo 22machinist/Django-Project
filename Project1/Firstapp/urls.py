866aa0a79f46842162ca4789d40df3e89089b1a6
Ashutosh Sharma <ashutosh-sharma-22@users.noreply.github.com>
2023-06-10 07:03:46 +0530

    Added Greets url

--- Project1/Firstapp/urls.py
+++ Project1/Firstapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path    #This will import path
 from . import views             #This will import views from the current directory 
 urlpatterns = [
     #Defining the path that will be rendered on the request
+    path("<str:name>" , views.greet , name="greet"),
     path("",views.index, name="index"),
     path("Ashutosh" , views.Ashutosh , name="Ashutosh"),
     path("David" , views.David , name="David") , 
