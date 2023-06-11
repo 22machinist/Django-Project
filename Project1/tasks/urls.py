a742f7b466b63a833694f78f60998928e6131499
Ashutosh Sharma <ashutosh-sharma-22@users.noreply.github.com>
2023-06-11 19:11:28 +0530

    ADded app-name

--- Project1/tasks/urls.py
+++ Project1/tasks/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path 
 from . import views
 
+#Adding the app name
+app_name = "tasks"
 urlpatterns = [
     path("" , views.index , name="index") ,
     #Adding the path for add function
