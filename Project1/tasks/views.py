0df91ea6b5eb673044cec16b6498d3c95f3007ea
Ashutosh Sharma <ashutosh-sharma-22@users.noreply.github.com>
2023-06-12 23:35:42 +0530

    Added the class

--- Project1/tasks/views.py
+++ Project1/tasks/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
+from django import forms
 tasks = ["Foo" , "Harry" , "George"]         #Defining the universal variable
 # Create your views here.
+
+#Adding a class to contain form
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task : ")
 
 def index(request):
     return render(request , "tasks/index.html" , {
@@ -11,4 +15,6 @@ def index(request):
 
 #Adding new function to add the new lists
 def add (request):
-    return render (request , "tasks/add.html" )
+    return render (request , "tasks/add.html" , {
+        "form" : NewTaskForm()
+    })
