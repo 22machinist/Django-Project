545448d8d2e3336df7ae222e98e440f8ae29e67f
Ashutosh Sharma <ashutosh-sharma-22@users.noreply.github.com>
2023-06-19 09:12:16 +0530

    changed/removed id

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -15,7 +15,7 @@ class Flight(models.Model):
     duration = models.IntegerField()
 
     def __str__(self):
-        return f"{self.id} : {self.origin} to {self.destination}"
+        return f"{self.origin} to {self.destination}"
     
 class Passenger(models.Model):
     first = models.CharField(max_length=64)
